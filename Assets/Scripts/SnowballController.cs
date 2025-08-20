1867942abfd0c1370d7d41b73138f8cc6afbba51
kristofer <aminopoda@gmail.com>
2025-08-20 17:54:11 +0400

    Fix healthbar

--- Assets/Scripts/SnowballController.cs
+++ Assets/Scripts/SnowballController.cs
@@ -16,9 +16,27 @@ public class SnowballController : MonoBehaviour
         Debug.Log("Snowball hit: " + collision.gameObject.name);
 
         Health health = collision.gameObject.GetComponent<Health>();
+        if (health == null)
+        {
+            health = collision.gameObject.GetComponentInParent<Health>();
+        }
         if (health != null)
         {
-            health.TakeDamage(damage);
+            health.TakeDamageFrom(gameObject, damage);
+        }
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Health health = other.GetComponent<Health>();
+        if (health == null)
+        {
+            health = other.GetComponentInParent<Health>();
+        }
+        if (health != null)
+        {
+            health.TakeDamageFrom(gameObject, damage);
         }
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/SnowballProjectile.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+// Маркерный компонент, обозначающий, что объект является снежком-снарядом
+public class SnowballProjectile : MonoBehaviour {}
+
+
